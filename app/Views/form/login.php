06fcbdc3a0778936f6c9c9b35bc946d9f6502608
Umairah Sabri <n.umairahsabri@gmail.com>
2026-02-11 12:38:55 +0800

    change tab title

--- app/Views/form/login.php
+++ app/Views/form/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log Masuk | ICT4U</title>
+    <title>Log Masuk</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
