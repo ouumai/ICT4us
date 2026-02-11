8f6f17cbbf91277151aeea4a8ef94b7f82cea24b
Umairah Sabri <n.umairahsabri@gmail.com>
2026-02-12 07:57:32 +0800

    add routes group in authentication routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -108,23 +108,28 @@ $routes->group('users', ['namespace' => 'App\Controllers'], function($routes){
 
 
 // --- Authentication ---
-$routes->get('/register', 'Auth::register');
-$routes->post('/register', 'Auth::attemptRegister');
-$routes->get('/login', 'Auth::login');
-$routes->post('/login', 'Auth::attemptLogin');
-$routes->get('/logout', 'Auth::logout');
+// Paksa dia guna namespace App\Controllers supaya dia jumpa Auth.php
+$routes->group('', ['namespace' => 'App\Controllers'], function($routes) 
+{
+    $routes->get('register', 'Auth::register');
+    $routes->post('register', 'Auth::attemptRegister');
+    $routes->get('login', 'Auth::login');
+    $routes->post('login', 'Auth::attemptLogin');
+    $routes->get('logout', 'Auth::logout');
 
-// --- Profile Management ---
-$routes->get('profile', 'Auth::profile');
-$routes->post('profile/update', 'Auth::updateProfile');
-$routes->post('profile/update-password', 'Auth::updatePassword');
-$routes->get('get-profile-pic/(:any)', 'Auth::getFile/$1');
-$routes->get('profile/delete-pic', 'Auth::deleteProfilePic');
+    // --- Profile Management ---
+    $routes->get('profile', 'Auth::profile');
+    $routes->post('profile/update', 'Auth::updateProfile');
+    $routes->post('profile/update-password', 'Auth::updatePassword');
+    $routes->get('get-profile-pic/(:any)', 'Auth::getFile/$1');
+    $routes->get('profile/delete-pic', 'Auth::deleteProfilePic');
 
-// --- Direct Password Reset (Tiada Emel) ---
-$routes->get('forgot-password', 'Auth::forgotPassword');
-// Rute di bawah akan memproses pertukaran password terus dari borang
-$routes->post('forgot-password', 'Auth::attemptDirectReset');
+    // --- Direct Password Reset (Tiada Emel) ---
+    $routes->get('forgot-password', 'Auth::forgotPassword');
+    // Route di bawah akan memproses pertukaran password terus dari borang
+    $routes->post('forgot-password', 'Auth::attemptDirectReset');
+
+});
 
 // --------------------------------------------------------------------
 // Servis Kelulusan
